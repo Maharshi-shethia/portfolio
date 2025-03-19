5a7e120f105049152adf4a69d36f551ea2f31987
Maharshi Shethia <58566527+Maharshi-shethia@users.noreply.github.com>
2025-03-19 16:46:44 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".stack-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
